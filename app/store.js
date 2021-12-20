4ce725068bdcdfb8cd5877d95bc46b59598f1257
Adegoke Temitope <adegoketemitope1909@gmail.com>
2021-12-20 12:30:35 +0100

    remove all next files

--- app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import sidebarReducer from "../features/sidebar/sidebarSlice";
-
-export const store = configureStore({
-  reducer: {
-    sidebar: sidebarReducer,
-  },
-  devTools: process.env.NODE_ENV !== "production",
-});
