1f37ac8b2e5e96c7137f9390f42c84a4bca757a4
Adegoke Temitope <adegoketemitope1909@gmail.com>
2023-04-24 21:34:50 +0100

    Move title to _app
    Move title to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import '@/style/index.scss'
+import Head from "next/head";
+
+import "@/styles/globals.css";
+import "@/style/index.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Adegoke Temitope - Software Engineer</title>
+        <meta
+          name="description"
+          content="Adegoke Temitope is a software engineer based in Akure, Nigeria. He is passionate about building scalable and maintainable software."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Adegoke Temitope - Software Engineer</title>
-        <meta
-          name="description"
-          content="Adegoke Temitope is a software engineer based in Akure, Nigeria. He is passionate about building scalable and maintainable software."
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
